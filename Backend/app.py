dffb6389f66d934967e08acd729e4ec59cd6a790
unknown <jrbs2501@gmail.com>
2025-04-15 17:02:24 -0500

    testing database

--- Backend/app.py
+++ Backend/app.py
@@ -1,24 +1,43 @@
-import flask as fl
+from flask import Flask, session, render_template, url_for, request, redirect
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import Integer, String
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
 
-app = fl.Flask(__name__)
+
+app = Flask(__name__)
 app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///project.db"
+
+class Base(DeclarativeBase):
+  pass
+
+db = SQLAlchemy(model_class=Base)
+db.init_app(app)
+
+class User(db.Model):
+    id: Mapped[int] = mapped_column(primary_key=True)
+    username: Mapped[str] = mapped_column(unique=True)
+    password: Mapped[str]
+
+with app.app_context():
+    db.create_all()
 
 @app.route('/')
 def index():
     username = None
-    if 'username' in fl.session:
-        username = fl.session["username"]
-    return fl.render_template("index.html",person = username)
+    if 'username' in session:
+        username = session["username"]
+    return render_template("index.html",person = username)
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
-    if fl.request.method == 'POST':
-        fl.session['username'] = fl.request.form['username']
-        return fl.redirect(fl.url_for('index'))
-    return fl.render_template("login.html")
+    if request.method == 'POST':
+        session['username'] = request.form['username']
+        return redirect(url_for('index'))
+    return render_template("login.html")
 
 @app.route('/logout')
 def logout():
     # remove the username from the session if it's there
-    fl.session.pop('username', None)
-    return fl.redirect(fl.url_for('index'))
+    session.pop('username', None)
+    return redirect(url_for('index'))
